2b686e02fd380125f2f2c8c7ffc8e495b84e8a0b
senuravihanjayadeva <senurajayadeva@gmail.com>
2025-02-23 20:47:11 +0530

    Changes in services

--- src/services/posts.services.ts
+++ src/services/posts.services.ts
@@ -1,21 +1,23 @@
 import axios from "axios"
 
+const BASE_URL = "https://postms-g2cvgfeeg5cfbsdn.centralindia-01.azurewebsites.net"; // Here replace your backend url
+
 export const fetchAllPosts = async () => {
-   const response = await axios.get("https://postms-g2cvgfeeg5cfbsdn.centralindia-01.azurewebsites.net/api/v1/posts");
+   const response = await axios.get(`${BASE_URL}/api/v1/posts`);
    return response.data
 }
 
 export const createPosts = async (post: IPost) => {
-   const response = await axios.post("https://postms-g2cvgfeeg5cfbsdn.centralindia-01.azurewebsites.net/api/v1/create", post);
+   const response = await axios.post(`${BASE_URL}/api/v1/create`, post);
    return response.data.data
 }
 
 export const editPosts = async (id: string, post:IPost) => {
-   const response = await axios.put(`https://postms-g2cvgfeeg5cfbsdn.centralindia-01.azurewebsites.net/api/v1/posts/post/${id}`, post);
+   const response = await axios.put(`${BASE_URL}/api/v1/posts/post/${id}`, post);
    return response.data.data
 }
 
 export const deletePosts = async (id: string) => {
-   const response = await axios.delete(`https://postms-g2cvgfeeg5cfbsdn.centralindia-01.azurewebsites.net/api/v1/posts/post/${id}`);
+   const response = await axios.delete(`${BASE_URL}/api/v1/posts/post/${id}`);
    return response.data.data
 }
